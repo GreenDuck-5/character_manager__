34ccc50ca35fd94de3a84e3b16e97b64e3b43578
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2026-01-21 09:52:25 -0700

    Character creation pseudocode

---
+++ create_character.py
@@ -0,0 +1,9 @@
+#AP 1st Period, Character Manager Pseudocode
+
+#create function for creating character
+    #Make a list for attributes, all of which will be 0 and in a predetermined spot so we can use the same index to know which number is which
+    #Make another list for name, race, gender, and class, which will also have predetermined spots for which index they'll go in
+    #Make a list for the inventory which will be blank to start
+    #Adjust the stats and inventory based off of their race, class, etc.
+    #Save all of it in a list so its together as one character
+    #return the list so you can access it outside of the function
