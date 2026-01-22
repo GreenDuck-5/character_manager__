640322f07351cc4f96a45d8df24eb5c3723eb9d7
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-01-22 09:38:15 -0700

    Main Pseudocode

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 #Main file containing user interface
+
+#Main function:
+
+    # Give options to do: Character Search, Character Create/Edit, Character Inventory, Exit
+
+    # If the user picks Character Search:
+        # Run Character Search Function
+        # Restart Function
+
+    # If the user picks Character Create/Edit:
+        # Run Character Create Function
+        # Restart Function
+
+    # If the user picks Character Inventory:
+        # Run Character Inventory Function
+        # Restart Function
+
+    # If the user picks Exit:
+        # Exit Program
+
+    # If the user picks none of those:
+        # Print: "Please enter valid input"
+        # Restart Function
+
+#Introduce the user
+#Loop
+    # Run Main Function
