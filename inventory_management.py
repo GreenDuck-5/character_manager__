94cdcb4df5f7703ef54278a1001621bc05ab7f92
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-23 09:12:56 -0700

    pseudo

---
+++ inventory_management.py
@@ -0,0 +1,11 @@
+# MR 1st Pseudocode for Activity Diagram RPG
+# Display INSTRUCTIONS for using the inventory and proabably ask them if the want to go to their inventory menu
+# Make a set for the items in the inventory
+# Functions of the iunstructions and askthem uf they want to go to  the menu or not
+# IF yes call the inventory functions
+# If the user chooses not take them back to the main function?
+# A FUNCTION for the Inventory Menu
+# def inventory()
+# Ask if they want to view, add , or remove from their inventory
+# We will display the options of inventory that they already have
+# Ask them what they want to d
