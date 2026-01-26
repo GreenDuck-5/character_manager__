7c2135683ac11e18f85835c97df6389fba9f5181
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-26 09:48:17 -0700

    working on code implementation

--- inventory_management.py
+++ inventory_management.py
@@ -1,5 +1,21 @@
 # MR 1st Pseudocode for Activity Diagram RPG
 # Display INSTRUCTIONS for using the inventory and probably ask them if the want to go to their inventory menu
+
+def inventory_menu():
+    print("Welcome to your inventory menu!")
+    print("You have the choice to go back to the main menu or stay")
+
+    print( "1. Go back to main Menu")
+    print( "2. Stay in Inventory Menu")
+    stay_or_go = input("What would you like to do: ")
+
+    if stay_or_go == '1':
+        main()
+    elif stay_or_go == '2':
+        invetory_options()
+    else:
+        print("Please enter a valid Number.")
+    pass
 # Make a set for the items in the inventory
 # Functions of the iunstructions and ask them if they want to go to  the menu or not
 # IF yes call the inventory functions
@@ -36,6 +52,6 @@
 # add()
 # Display the list
 # Display add in options for the inventory
-# Ask what they want to add- multiple choice
+# Ask what they want to add - multiple choice
 # Add it
 # display the new list
