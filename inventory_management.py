e8e3e998b4c4fd57bf0a1e8ba839eb88ba9796b9
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-27 09:46:05 -0700

    inventory management

--- inventory_management.py
+++ inventory_management.py
@@ -7,7 +7,7 @@ def inventory_menu():
 
     print( "1. Go back to main Menu")
     print( "2. Stay in Inventory Menu")
-    stay_or_go = input("What would you like to do: ")
+    stay_or_go = input(print("What would you like to do: "))
 
     if stay_or_go == '1':
         main()
@@ -20,38 +20,58 @@ def inventory_menu():
 # Functions of the iunstructions and ask them if they want to go to  the menu or not
 # IF yes call the inventory functions
 # If the user chooses not take them back to the main function?
-
+inventory_menu()
 # A SET FOR THE ITEMS
-
+inventory_items = {"starter weapn", "glass of water"}
+inventory_possibilities = { "sword", "Med pack", " Bow and Arrow"}
 # A FUNCTION for the Inventory Menu
 # def inventory()
 
 # Ask if they want to view, add , or remove from their inventory
 # We will display the options of inventory that they already have
 #ITEMS
+def inventory_options():
+    print("Hello, this is your inventory where you will find and store your items")
+    print("Would you like to add, view, or remove from your inventory? ")
 
-# Ask them what they want to do
-# IF user wants to view
-#call the view function
-
-#Elif User wants to add
-# call the add function
-
-#Elif user wants to remove
-# call the remove funtion
-
-#Else,
-#Enter something valid
-
-# remove() 
+view_add_remove = input(print("What would you like to do ?")) 
+#remove() 
+def remove():
+    pass
 # take the list and number it
 #Display the set
 # ask what they want to remove
 # After then display the new list
 
 # add()
+def add():
+    pass
 # Display the list
 # Display add in options for the inventory
 # Ask what they want to add - multiple choice
 # Add it
 # display the new list
+
+def view():
+    pass
+    
+# Ask them what they want to do
+while True:
+# IF user wants to view
+#call the view function
+    if view_add_remove == '1':
+        view()
+#Elif User wants to add
+# call the add function
+    elif view_add_remove == '2':
+        add()
+
+#Elif user wants to remove
+# call the remove funtion
+    elif view_add_more == '3':
+        remove()
+#Else,
+#Enter something valid
+    else:
+        print("please enter a valid value.")
+        break
