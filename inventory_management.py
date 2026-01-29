4e1ae0bc09546c3ac85e73d0841b1ccb6fa2317b
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-29 08:00:51 -0700

    Update inventory_management.py

--- inventory_management.py
+++ inventory_management.py
@@ -1,13 +1,17 @@
 # MR 1st Pseudocode for Activity Diagram RPG
 # Display INSTRUCTIONS for using the inventory and probably ask them if the want to go to their inventory menu
 
+def main():
+    print("\nYou are back at the main menu.\n")
+
+
 def inventory_menu():
     print("Welcome to your inventory menu!")
     print("You have the choice to go back to the main menu or stay")
 
-    print( "1. Go back to main Menu")
-    print( "2. Stay in Inventory Menu")
-    stay_or_go = input(print("What would you like to do: "))
+    print("1. Go back to main Menu")
+    print("2. Stay in Inventory Menu")
+    stay_or_go = input("What would you like to do: ")
 
     if stay_or_go == '1':
         main()
@@ -15,15 +19,19 @@ def inventory_menu():
         inventory_options()
     else:
         print("Please enter a valid Number.")
+        inventory_menu()
     pass
+
+
 # Make a set for the items in the inventory
 # Functions of the iunstructions and ask them if they want to go to  the menu or not
 # IF yes call the inventory functions
 # If the user chooses not take them back to the main function?
-inventory_menu()
+
 # A SET FOR THE ITEMS
 inventory_items = {"starter weapon", "glass of water"}
-inventory_possibilities = { "sword", "Med pack", " Bow and Arrow"}
+inventory_possibilities = {"sword", "Med pack", "Bow and Arrow"}
+
 # A FUNCTION for the Inventory Menu
 # def inventory()
 
@@ -31,49 +39,106 @@ inventory_possibilities = { "sword", "Med pack", " Bow and Arrow"}
 # We will display the options of inventory that they already have
 #ITEMS
 def inventory_options():
-    print("Hello, this is your inventory where you will find and store your items")
-    print("Would you like to add, view, or remove from your inventory? ")
+    while True:
+        print("Hello, this is your inventory where you will find and store your items")
+        print("1. View Inventory")
+        print("2. Add Item")
+        print("3. Remove Item")
+        print("4. Exit Inventory")
 
-view_add_remove = input(print("What would you like to do ?")) 
+        view_add_remove = input("What would you like to do ? ")
+
+        # Ask them what they want to do
+        # IF user wants to view
+        #call the view function
+        if view_add_remove == '1':
+            view()
+
+        #Elif User wants to add
+        # call the add function
+        elif view_add_remove == '2':
+            add()
+
+        #Elif user wants to remove
+        # call the remove funtion
+        elif view_add_remove == '3':
+            remove()
+
+        #Else,
+        #Enter something valid
+        elif view_add_remove == '4':
+            inventory_menu()
+            break
+        else:
+            print("please enter a valid value.")
+
+
 #remove() 
 def remove():
     print(inventory_items)
+
+    # take the list and number it
+    item_list = list(inventory_items)
+
+    for i in range(len(item_list)):
+        print(i + 1, item_list[i])
+
+    #Display the set
+    # ask what they want to remove
+    choice = input("What number do you want to remove? ")
+
+    if choice.isdigit():
+        choice = int(choice)
+
+        if 1 <= choice <= len(item_list):
+            removed_item = item_list[choice - 1]
+            inventory_items.remove(removed_item)
+            print("Removed:", removed_item)
+        else:
+            print("Invalid number.")
+
+    # After then display the new list
+    print("Updated Inventory:", inventory_items)
     pass
-# take the list and number it
-#Display the set
-# ask what they want to remove
-# After then display the new list
+
 
 # add()
 def add():
     print(inventory_items)
+
+    # Display the list
+    possible_list = list(inventory_possibilities)
+
+    # Display add in options for the inventory
+    for i in range(len(possible_list)):
+        print(i + 1, possible_list[i])
+
+    # Ask what they want to add - multiple choice
+    choice = input("What number do you want to add? ")
+
+    if choice.isdigit():
+        choice = int(choice)
+
+        if 1 <= choice <= len(possible_list):
+            selected_item = possible_list[choice - 1]
+            inventory_items.add(selected_item)
+            print("Added:", selected_item)
+        else:
+            print("Invalid number.")
+
+    # Add it
+    # display the new list
+    print("Updated Inventory:", inventory_items)
     pass
-# Display the list
-# Display add in options for the inventory
-# Ask what they want to add - multiple choice
-# Add it
-# display the new list
+
 
 def view():
+    # Display the set
+    print("Your Inventory:")
+    for item in inventory_items:
+        print(item)
     pass
-    
-# Ask them what they want to do
-while True:
-# IF user wants to view
-#call the view function
-    if view_add_remove == '1':
-        view()
-#Elif User wants to add
-# call the add function
-    elif view_add_remove == '2':
-        add()
 
-#Elif user wants to remove
-# call the remove funtion
-    elif view_add_more == '3':
-        remove()
-#Else,
-#Enter something valid
-    else:
-        print("please enter a valid value.")
-        break
+
+inventory_menu()
+
